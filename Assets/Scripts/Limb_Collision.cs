356fcc36814f87b2c246a508e55cac804e922c27
EwanSmith4001418 <71463995+EwanSmith4001418@users.noreply.github.com>
2022-04-28 14:42:20 +0100

    Movement and Jump

---
+++ Assets/Scripts/Limb_Collision.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limb_Collision : MonoBehaviour
+{
+    public Player_Controller playerController;
+
+    private void Start()
+    {
+        playerController = GameObject.FindObjectOfType<Player_Controller>().GetComponent<Player_Controller>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        playerController.isGrounded = true;
+    }
+}
--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
+    public float speed;
+    public float strafespeed;
+    public float jumpForce;
+
+    public Rigidbody Pelvis;
+    public bool isGrounded;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Pelvis = GetComponent<Rigidbody>();   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            Pelvis.AddForce(Pelvis.transform.forward * speed);
+        }
+
+
+        if(Input.GetAxis("Jump")>0)
+        {
+            if (isGrounded)
+            {
+                Pelvis.AddForce(new Vector3(0, jumpForce, 0));
+                isGrounded = false;
+            }
+        }
     }
 }
