0135fc2bf38fe16aba3101d9c2b1efc32082cc04
ReeseTodhunter <reeset538@gmail.com>
2022-04-28 11:27:35 +0100

    Added CargoSpawner Gizmo for viewing size

--- Assets/Scripts/CargoSpawner.cs
+++ Assets/Scripts/CargoSpawner.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class CargoSpawner : MonoBehaviour
 {
+    private const int WIDTH = 5, LENGTH = 8;
+
     public GameObject[] smallCrate;
     public GameObject[] longCrate;
     public GameObject[] largeCrate;
 
     public int numOfSmall, numOfLong, numOfLarge;
-    [Space]
-    public int Width, Length;
 
     void Start()
     {
@@ -20,5 +20,11 @@ public class CargoSpawner : MonoBehaviour
     public void SpawnCargo(int amountSmall, int amountLong, int amountLarge)
     {
 
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(this.transform.position, new Vector3(WIDTH, 3.0f, LENGTH));
     }
 }
