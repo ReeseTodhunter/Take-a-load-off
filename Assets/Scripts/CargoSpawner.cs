f70934238c7bc95cecc0fe6f98155e684516da33
ReeseTodhunter <reeset538@gmail.com>
2022-04-28 10:28:12 +0100

    Camera Position Moved + CargoSpawner Script Started

---
+++ Assets/Scripts/CargoSpawner.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CargoSpawner : MonoBehaviour
+{
+    public GameObject[] smallCrate;
+    public GameObject[] longCrate;
+    public GameObject[] largeCrate;
+}
