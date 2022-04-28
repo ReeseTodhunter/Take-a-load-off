6bb6e89e7e6035a73c15e411de68a0d4172d42d9
ReeseTodhunter <reeset538@gmail.com>
2022-04-28 11:20:25 +0100

    Scene Rearranged

--- Assets/Scripts/CargoSpawner.cs
+++ Assets/Scripts/CargoSpawner.cs
@@ -7,4 +7,18 @@ public class CargoSpawner : MonoBehaviour
     public GameObject[] smallCrate;
     public GameObject[] longCrate;
     public GameObject[] largeCrate;
+
+    public int numOfSmall, numOfLong, numOfLarge;
+    [Space]
+    public int Width, Length;
+
+    void Start()
+    {
+
+    }
+
+    public void SpawnCargo(int amountSmall, int amountLong, int amountLarge)
+    {
+
+    }
 }
