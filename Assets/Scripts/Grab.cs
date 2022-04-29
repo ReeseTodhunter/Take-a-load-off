b89de66d0652b6c4ad163f6502e648fdeee9d38e
EwanSmith4001418 <71463995+EwanSmith4001418@users.noreply.github.com>
2022-04-29 03:34:57 +0100

    Grab works

--- Assets/Scripts/Grab.cs
+++ Assets/Scripts/Grab.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class Grab : MonoBehaviour
 {
+    public Animator animator;
+    GameObject grabbedObj;
+    public Rigidbody rb;
+    public int isLeftorRight;
+    public bool alreadyGrabbing = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(isLeftorRight))
+        {
+            if(isLeftorRight == 0)
+            {
+                animator.SetBool("isLeftHandUp", true);
+            }
+            else if (isLeftorRight == 1)
+            {
+                animator.SetBool("isRightHandUp", true);
+            }
+
+            FixedJoint fj = grabbedObj.AddComponent<FixedJoint>();
+            fj.connectedBody = rb;
+            fj.breakForce = 9001;
+        }
+        else if(Input.GetMouseButtonUp(isLeftorRight))
+        {
+            if(isLeftorRight == 0)
+            {
+                animator.SetBool("isLeftHandUp", false);
+            }
+            else if (isLeftorRight == 1)
+            {
+                animator.SetBool("isRightHandUp", false);
+            }
+
+            if(grabbedObj != null)
+            {
+                Destroy(grabbedObj.GetComponent<FixedJoint>());
+            }
+
+            grabbedObj = null;
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Cargo>() as Cargo != null)
+        {
+            grabbedObj = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        grabbedObj = null;
+
     }
 }
