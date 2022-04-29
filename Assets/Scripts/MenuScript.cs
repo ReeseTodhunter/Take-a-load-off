22553a29139c870e13719b33d588758b7366ee59
EwanSmith4001418 <71463995+EwanSmith4001418@users.noreply.github.com>
2022-04-29 05:04:33 +0100

    Main Menu Started

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public void Playgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
     // Start is called before the first frame update
     void Start()
     {
